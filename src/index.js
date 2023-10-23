ed1c3960e4a89d67d6be0a0678e5bbfb91d96f99
thegeeksayf <148209818+thegeeksayf@users.noreply.github.com>
2023-10-23 14:23:38 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar.jsx";
+import Billing from "./components/Billing.jsx";
+import CardDeal from "./components/CardDeal.jsx";
+import Business from "./components/Business.jsx";
+import Clients from "./components/Clients.jsx";
+import CTA from "./components/CTA.jsx";
+import Stats from "./components/Stats.jsx";
+import Footer from "./components/Footer.jsx";
+import Testimonials from "./components/Testimonials.jsx";
+import Hero from "./components/Hero.jsx";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
